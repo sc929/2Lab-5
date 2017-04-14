24e0e619740d7ce7c7281bd4007a40b2e03c4e91
sc929 <sc929@yandex.ru>
2017-04-14 19:36:16 +0400

    Update 2Lab-5.hpp

--- include/2Lab-5.hpp
+++ include/2Lab-5.hpp
@@ -9,7 +9,7 @@ class BinarySearchTree
 
 public: 
 	int data;
-	int Add(unsigned int new_data, BinarySearchTree *tree);
+	int Add(int new_data, BinarySearchTree *tree);
 	int Print(int check1, BinarySearchTree *tree);
 	int Search(int check2, BinarySearchTree *tree);
 	int Deletion(int check3, BinarySearchTree *tree);
