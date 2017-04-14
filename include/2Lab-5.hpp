a7d222858fc07e5d9f8d83296ab674b84e65eab9
sc929 <sc929@yandex.ru>
2017-04-14 10:22:55 +0300

    Добавлена проверка в Travis

--- include/2Lab-5.hpp
+++ include/2Lab-5.hpp
@@ -10,7 +10,7 @@ public:
 	BinarySearchTree *parent; 
 	unsigned int data;
 	int Add(unsigned int new_data, BinarySearchTree *&tree);
-	int print(int check1, BinarySearchTree *tree);
+	int Print(int check1, BinarySearchTree *tree);
 	int Search(int check2, BinarySearchTree *tree);
 	int Deletion(int check3, BinarySearchTree *tree);
 	int FreeTree(int check4, BinarySearchTree *&tree);
