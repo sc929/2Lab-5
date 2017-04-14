9b815366b395b2c79b4e5ac00341c097a4de8bec
sc929 <sc929@yandex.ru>
2017-04-14 19:07:40 +0400

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -2,36 +2,36 @@
 #include <catch.hpp>
 
 SCENARIO("Add") {
-    BinarySearchTree r;
-    BinarySearchTree* tree;
+    BinarySearchTree<int> r;
+    BinarySearchTree<int>* tree;
     int rv = r.Add(10, tree);
     
     REQUIRE( rv == 10);
 }
 SCENARIO("Print") {
-    BinarySearchTree r;
-    BinarySearchTree* tree;
+    BinarySearchTree<int> r;
+    BinarySearchTree<int>* tree;
     int rv = r.Print(11, tree);
     
     REQUIRE( rv == 11);
 }
 SCENARIO("Search") {
-    BinarySearchTree r;
-    BinarySearchTree* tree;
+    BinarySearchTree<int> r;
+    BinarySearchTree<int>* tree;
     int rv = r.Search(12, tree);
     
     REQUIRE( rv == 12);
 }
 SCENARIO("Delete_branch") {
-    BinarySearchTree r;
-    BinarySearchTree* tree;
+    BinarySearchTree<int> r;
+    BinarySearchTree<int>* tree;
     int rv = r.Search(13, tree);
     
     REQUIRE( rv == 13);
 }
 SCENARIO("FreeTree") {
-    BinarySearchTree r;
-    BinarySearchTree* tree;
+    BinarySearchTree<int> r;
+    BinarySearchTree<int>* tree;
     int rv = r.Search(14, tree);
     
     REQUIRE( rv == 14);
