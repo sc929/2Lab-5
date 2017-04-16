01ff6c7575da94efeb8ecd944a15a72256f16834
sc929 <sc929@yandex.ru>
2017-04-16 20:19:06 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -3,35 +3,35 @@
 
 SCENARIO("Add") {
     BinarySearchTree<int> r;
-    BinarySearchTree<int> *tree;
+    BinarySearchTree<int>* tree;
     int rv = r.Add(10, tree);
     
     REQUIRE( rv == 10);
 }
 SCENARIO("Print") {
     BinarySearchTree<int> r;
-    BinarySearchTree<int> *tree;
+    BinarySearchTree<int>* tree;
     int rv = r.Print(11, tree);
     
     REQUIRE( rv == 11);
 }
 SCENARIO("Search") {
     BinarySearchTree<int> r;
-    BinarySearchTree<int> *tree;
+    BinarySearchTree<int>* tree;
     int rv = r.Search(12, tree);
     
     REQUIRE( rv == 12);
 }
 SCENARIO("Delete_branch") {
     BinarySearchTree<int> r;
-    BinarySearchTree<int> *tree;
+    BinarySearchTree<int>* tree;
     int rv = r.Search(13, tree);
     
     REQUIRE( rv == 13);
 }
 SCENARIO("FreeTree") {
     BinarySearchTree<int> r;
-    BinarySearchTree<int> *tree;
+    BinarySearchTree<int>* tree;
     int rv = r.Search(14, tree);
     
     REQUIRE( rv == 14);
